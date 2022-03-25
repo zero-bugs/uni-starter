da4e582d1d7be080af0ab58f244b29781813eb77
annoymous <annoymous@example.com>
2022-03-25 22:22:27 +0800

    更新数据库布局

--- script.ts
+++ script.ts
@@ -4,9 +4,7 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-    const allUsers = await prisma.user.findMany({
-        include: { posts: true },
-    })
+    const allUsers = await prisma.image.findMany();
     // use `console.dir` to print nested objects
     console.dir(allUsers, { depth: null })
 }
