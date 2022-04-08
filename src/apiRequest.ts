2a59387ab131d1f1244b2ea9290375dd224381e9
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2022-04-08 20:00:21 +0800

    Update apiRequest.ts

--- src/apiRequest.ts
+++ src/apiRequest.ts
@@ -1,12 +1,29 @@
-import fetch from 'node-fetch';
-import {parentPort} from 'worker_threads';
+import fetch, {RequestInit} from 'node-fetch';
+import {parentPort, threadId} from 'worker_threads';
+
+import {PrismaClient, ImageCreateInput} from '@prisma/client';
 
 import CustomEvent from "./config/customEvent.js";
 
-import {getLogger} from "./@utils/utils.js";
+import {delay, getDbClient, getLogger} from "./@utils/utils.js";
 import {getHttpsProxy} from "./config/proxyConfig.js";
+import {randomInt} from "crypto";
 
 let logger4js = getLogger('app')
+
+class ResultResp {
+    type: string;
+    url: string;
+    httpCode: number;
+    threadId: number;
+    
+    constructor(type: string, url: string, status: number, threadId: number) {
+        this.type = type;
+        this.url = url;
+        this.httpCode = status;
+        this.threadId = threadId;
+    }
+}
 
 /**
  * wh: default search
@@ -20,26 +37,40 @@ export async function whSearchListDefault(endpoint: string, queryParam = {
     "startPage": 1,
     "endPage": 1
 }) {
-    while (queryParam.startPage < queryParam.endPage) {
-        await fetch(endpoint, {
-            agent: getHttpsProxy(),
-        }).then(res => {
+    let page = queryParam.startPage === 0 ? 1 : queryParam.startPage;
+    let urlLink = `${endpoint}?purity=${queryParam.purity}&sorting=${queryParam.sorting}&order=${queryParam.order}`
+    while (page < queryParam.endPage) {
+        let pageUrlLink = `${urlLink}&page=${page}`
+        page += 1;
+        
+        let options: RequestInit = {};
+        let proxy = getHttpsProxy();
+        if (proxy) {
+            options.agent = getHttpsProxy();
+        }
+        
+        await fetch(pageUrlLink, options).then(res => {
+            let result = new ResultResp(CustomEvent.CLIENT_ERR, pageUrlLink,
+                res.status, threadId);
             if (res.status !== 200 && res.status !== 201) {
-                parentPort?.emit(CustomEvent.CLIENT_ERR, [{
-                    'url': endpoint,
-                    'status': res.status
-                }])
+                parentPort?.postMessage(result);
                 logger4js.warn('worker execute failed request url:%s', endpoint)
                 throw new Error(`http error:${res.status}, ${res.body}`);
             }
-            parentPort?.emit(CustomEvent.CLIENT_RES, [{
-                'url': endpoint,
-                'status': res.status
-            }])
+            parentPort?.postMessage(result);
             return res.json();
-        }).then((res)=>{
-            setTimeout(() => {}, 5000);
+        }).then(res => {
+            let imgs: ImageCreateInput[] = [
+            
+            ];
+            
+            
+            // put into db
+            let pmsClient = getDbClient();
+        }).catch(e => {
+            logger4js.warn("http request error for url:%s", pageUrlLink, e);
         });
+        
+        await delay(randomInt(3000, 6000));
     }
 }
-
