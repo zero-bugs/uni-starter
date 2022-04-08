27782c2e901d817a7ffd9feff7f7423643bf2e21
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2022-04-08 20:01:06 +0800

    Update multiThreads.ts

--- src/multiThreads.ts
+++ src/multiThreads.ts
@@ -1,11 +1,9 @@
 import {isMainThread, parentPort, Worker, workerData} from 'worker_threads';
-import fs from "fs";
 import {fileURLToPath} from 'url'
 
 import {whSearchListDefault} from './apiRequest.js'
 import {configure, getLog4jsConfFile, getLogger} from "./@utils/utils.js";
 import {ApiKeyId, getApiEndpoint} from "./config/configFile.js";
-import CustomEvent from "./config/customEvent";
 
 const __filename = fileURLToPath(import.meta.url)
 
@@ -15,10 +13,10 @@ configure(getLog4jsConfFile());
 let logger = getLogger(__filename)
 
 function mainThread() {
-    let step = 10;
+    let step = 5;
     let startIndex = 0;
     let endIndex = step;
-
+    
     for (let i = 0; i < 3; ++i) {
         startIndex = startIndex + i * step;
         endIndex = endIndex + startIndex;
@@ -31,17 +29,12 @@ function mainThread() {
                 "endPage": endIndex
             }
         });
+        
         worker.on('exit', code => {
             console.log(`main: worker stopped with exit code ${code}`);
         });
         worker.on('message', msg => {
-            console.log(`main: receive message: ${msg}`);
-        });
-        worker.on(CustomEvent.CLIENT_RES.valueOf(), msg => {
-            console.log(`main: receive ${CustomEvent.CLIENT_RES}: ${JSON.stringify(msg)}`);
-        })
-        worker.on(CustomEvent.CLIENT_ERR.valueOf(), msg => {
-            console.log(`main: receive ${CustomEvent.CLIENT_ERR}: ${JSON.stringify(msg)}`);
+            console.log(`main: receive message: ${JSON.stringify(msg)}`);
         });
     }
 }
