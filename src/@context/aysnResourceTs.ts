28f8dc85c69dc655571fe1e54af3b6198c833127
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2022-05-08 18:06:20 +0800

    Create aysnResourceTs.ts

---
+++ src/@context/aysnResourceTs.ts
@@ -0,0 +1,21 @@
+import {AsyncResource} from "async_hooks";
+
+class DBQuery extends AsyncResource {
+    db: any;
+    
+    constructor(db: any) {
+        super('DBQuery');
+        this.db = db;
+    }
+    
+    getInfo(query: string, callback: any) {
+        this.db.get(query, (err: any, data: any) => {
+            this.runInAsyncScope(callback, null, err, data);
+        });
+    }
+    
+    close() {
+        this.db = null;
+        this.emitDestroy();
+    }
+}
