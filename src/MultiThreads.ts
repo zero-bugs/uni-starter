74b1cc841353c62f2b1255aca662d32b60e37be9
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2022-05-08 17:59:31 +0800

    Update and rename multiThreads.ts to MultiThreads.ts

---
+++ src/MultiThreads.ts
@@ -0,0 +1,24 @@
+import {ApiKeyId, getApiKey} from "./config/ConfigFile.js";
+import {WorkerPool, WorkerTaskInfo} from "./@pool/WorkPool.js";
+
+const POOL_SIZE = 3;
+
+export async function main() {
+    const pool = new WorkerPool(POOL_SIZE, 100);
+    let step = 1100;
+    let startIndex = -step;
+    let endIndex = 0;
+    for (let i = 0; i < POOL_SIZE; ++i) {
+        startIndex = startIndex + step;
+        endIndex = endIndex + step;
+        pool.runTask(new WorkerTaskInfo(ApiKeyId.WH_QUERY_01, {
+            purity: '011',
+            category: '111',
+            apikey: `${getApiKey()}`,
+            sorting: "date_added",
+            order: "desc",
+            startPage: startIndex,
+            endPage: endIndex,
+        }));
+    }
+}
