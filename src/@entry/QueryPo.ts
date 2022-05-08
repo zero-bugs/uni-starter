1c0258e482c6099b1070799cace5c1dc1135cae4
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2022-05-08 18:04:27 +0800

    Create QueryPo.ts

---
+++ src/@entry/QueryPo.ts
@@ -0,0 +1,11 @@
+type QueryParam = {
+    purity: String;
+    category: string;
+    apikey: string;
+    sorting: String,
+    order: string,
+    startPage: number,
+    endPage: number,
+    sinceBegin: Date, //2022-03-19 00:15:04
+    sinceEnd: Date
+}
