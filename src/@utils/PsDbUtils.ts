22c5fd8acdbc87757e526adff28b4375db9a798e
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2022-05-08 18:00:41 +0800

    Create PsDbUtils.ts

---
+++ src/@utils/PsDbUtils.ts
@@ -0,0 +1,40 @@
+import {ImgEntryPo} from "../@entry/ImgEntryPo.js";
+import {pmsClient} from "./Utils.js";
+
+/**
+ * create before check exist or not.
+ * @param entry
+ */
+export async function pmsCreateWithCheckExist(entry: ImgEntryPo): Promise<boolean> {
+    const count = await pmsClient.image.count({
+        where: {
+            imgId: entry.id,
+        }
+    });
+    
+    if (count !== 0) {
+        return false;
+    }
+    
+    await pmsClient.image.create({
+        data: {
+            imgId: entry.id,
+            fileType: entry.file_type,
+            fileSize: entry.file_size,
+            dimensionX: entry.dimension_x,
+            dimensionY: entry.dimension_y,
+            purity: entry.purity,
+            category: entry.category,
+            path: entry.path,
+            url: entry.url,
+            source: entry.source,
+            views: entry.views,
+            favorites: entry.favorites,
+            ratio: entry.ratio,
+            createdTime: new Date(entry.created_at),
+        }
+    });
+    return true;
+}
+
+
